525371bd599a283085fd4c580e32b34361f9d290
zvovov <zvovov@gmail.com>
2016-10-14 21:42:54 +0530

    unformatted incoming msgs displayed

--- chat.py
+++ chat.py
@@ -1,6 +1,9 @@
 #! /usr/bin/python3
 
+import sched
 import sys
+import threading
+import time
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -10,24 +13,29 @@ from selenium.webdriver.common.action_chains import ActionChains
 # WW = Whatsapp Web = web.whatsapp.com
 wwUrl = "https://web.whatsapp.com/"
 
+# for incoming msgs
+getMsgInterval = 5
+incomingScheduler = sched.scheduler(time.time, time.sleep)
+
 
 def main():
-    # setting up Chrome with selenium
-    driver = webdriver.Chrome('/home/chirag/src/chromedriver/chromedriver')
-
-    # open WW in browser
-    driver.get(wwUrl)
-
-    # prompt user to connect device to WW
-    while True:
-        isConnected = input("\n\t Phone connected? y/n: ")
-        if isConnected.lower() == 'y':
-            break
-
-    assert "WhatsApp" in driver.title
-
-    # search name of friend/group
     if len(sys.argv) > 1:
+        # setting up Chrome with selenium
+        driver = webdriver.Chrome('/home/chirag/src/chromedriver/chromedriver')
+
+        # open WW in browser
+        driver.get(wwUrl)
+
+        # prompt user to connect device to WW
+        while True:
+            isConnected = input("\n\t Phone connected? y/n: ")
+            if isConnected.lower() == 'y':
+                break
+
+        assert "WhatsApp" in driver.title
+
+        # search name of friend/group
+
         friendName = ' '.join(sys.argv[1:])
         searchBar = driver.find_element(By.TAG_NAME, "input")
         searchBar.send_keys(friendName)
@@ -36,27 +44,60 @@ def main():
         # getting true name of contact/group
         threadName = driver.find_element_by_xpath('//*[@id="main"]/header//span[contains(text(), {})]'.format(friendName))
 
+        # TODO: verify threadName after every 10 msgs or 30 seconds
+
         # sending multiple msgs to friendName
-        print("Sending msgs to: ", threadName.text)
+        print("\n\tSending msgs to:", threadName.text)
+
+        # start background thread
+        incomingThread = threading.Thread(target=startGetMsg, args=(driver,))
+        incomingThread.start()
+
         while True:
             msg = input().strip()
             if msg == 'fuckoffnow' or msg == 'stopit':
                 print("\t Press Enter to end.")
+                # TODO: stop the incomingScheduler event
                 break
             sendMsg(driver, msg)
+
+    else:
+        sys.exit("\nError: Missing name of contact/group\npython chat.py <name>")
 
     # open all contacts page
     # driver.find_element(By.TAG_NAME, "button").click()
 
 
 def sendMsg(driver, msg):
-    '''
+    """
     Type 'msg' in 'driver' and press RETURN
-    '''
+    """
     action = ActionChains(driver)
     action.send_keys(msg)
     action.send_keys(Keys.RETURN)
     action.perform()
+
+
+def startGetMsg(driver):
+    """
+    Start schdeuler that gets incoming msgs every getMsgInterval seconds
+    """
+    incomingScheduler.enter(getMsgInterval, 1, getMsg, (driver, incomingScheduler))
+    incomingScheduler.run()
+
+
+def getMsg(driver, scheduler):
+    """
+    Get incoming msgs from the driver repeatedly
+    """
+    # print("...")
+
+    # get incoming msgs
+    allMsgs = driver.find_element_by_xpath('//*[@id="main"]//div[contains(@class, "message-list")]')
+    print(allMsgs.text)
+
+    # add the task to the scheduler again
+    incomingScheduler.enter(getMsgInterval, 1, getMsg, (driver, scheduler,))
 
 
 if __name__ == '__main__':
